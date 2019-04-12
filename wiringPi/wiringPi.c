7320df466cd5f0adfa4c2009e6c34e17cb462816
TheNextLVL <admin@nextlvl.xyz>
2019-04-12 23:00:24 +0200

    Updating for various stuff and bugs. CM3+ and PiA3+
    * Updated to version 2.50
    * Added suport for CM3+ and PiA3+
    * Other fixes related to compilation
    
    These changes have been mirrored from Gordon Henderson's git (https://git.drogon.net/?p=wiringPi).
    
    Co-Authored-By: gordondrogon <gordondrogon@users.noreply.github.com>

--- wiringPi/wiringPi.c
+++ wiringPi/wiringPi.c
@@ -219,7 +219,7 @@ volatile unsigned int *_wiringPiTimerIrqRaw ;
 
 static volatile unsigned int piGpioBase = 0 ;
 
-const char *piModelNames [16] =
+const char *piModelNames [20] =
 {
   "Model A",	//  0
   "Model B",	//  1
@@ -234,9 +234,13 @@ const char *piModelNames [16] =
   "CM3",	// 10
   "Unknown11",	// 11
   "Pi Zero-W",	// 12
-  "Pi 3+",	// 13
-  "Unknown14",	// 14
+  "Pi 3B+",	// 13
+  "Pi 3A+",	// 14
   "Unknown15",	// 15
+  "CM3+",       // 16
+  "Unknown15",	// 17
+  "Unknown15",	// 18
+  "Unknown15",	// 19
 } ;
 
 const char *piRevisionNames [16] =
@@ -2346,7 +2350,7 @@ int wiringPiSetup (void)
 
   piBoardId (&model, &rev, &mem, &maker, &overVolted) ;
 
-  if ((model == PI_MODEL_CM) || (model == PI_MODEL_CM3))
+  if ((model == PI_MODEL_CM) || (model == PI_MODEL_CM3) || (model == PI_MODEL_CM3P))
     wiringPiMode = WPI_MODE_GPIO ;
   else
     wiringPiMode = WPI_MODE_PINS ;
